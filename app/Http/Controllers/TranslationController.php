6d33b4c59e8bb31fb02b65aafa4be99a70bb7c50
Eyesore123 <joni.putkinen@protonmail.com>
2025-05-17 13:12:47 +0300

    updates

--- app/Http/Controllers/TranslationController.php
+++ app/Http/Controllers/TranslationController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class TranslationController extends Controller
 {
-    //
+    public function translate(Request $request)
+    {
+        $request->validate([
+            'q' => 'required|string',
+            'source' => 'required|string',
+            'target' => 'required|string',
+        ]);
+
+        $response = Http::post('https://libretranslate.de/translate', [
+            'q' => $request->input('q'),
+            'source' => $request->input('source'),
+            'target' => $request->input('target'),
+            'format' => 'text',
+            'api_key' => '',  // Leave empty for public instance
+        ]);
+
+        if ($response->failed()) {
+            return response()->json(['error' => 'Translation failed'], 500);
+        }
+
+        return response()->json([
+            'translatedText' => $response->json()['translatedText'] ?? '',
+        ]);
+    }
 }
