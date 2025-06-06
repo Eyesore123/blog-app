87505c01e4d204570a3cf627cd803086d97acf04
Eyesore123 <joni.putkinen@protonmail.com>
2025-06-06 12:20:29 +0300

    add commonmarkconverter to render content as HTML from Markdown

--- app/Mail/NewPostNotification.php
+++ app/Mail/NewPostNotification.php
@@ -5,6 +5,7 @@ use App\Models\Post;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use League\CommonMark\CommonMarkConverter;
 
 class NewPostNotification extends Mailable
 {
@@ -27,19 +28,29 @@ class NewPostNotification extends Mailable
      *
      * @return $this
      */
+
     public function build()
     {
-        $htmlContent = "
+        $converter = new CommonMarkConverter([
+            'html_input' => 'escape',
+            'allow_unsafe_links' => false,
+        ]);
+        $htmlContent = $converter->convert($this->post->content);
+
+        $emailHtml = "
             <html>
                 <body>
+                    <h2>Here's the latest blog post from Joni&#39;s blog:</h2>
                     <h1>{$this->post->title}</h1>
-                    <p>{$this->post->content}</p>
-                    <a href='" . url('/posts/' . $this->post->slug) . "'>Read more</a>
+                    <div style='font-size:16px;line-height:1.6;max-width:700px;margin-bottom:2rem;'>
+                        {$htmlContent}
+                    </div>
+                    <a href='" . url('/posts/' . $this->post->slug) . "' style='color:#5800FF;'>Read more</a>
                 </body>
             </html>
         ";
 
-        return $this->subject('New Post: ' . $this->post->title)
-                    ->html($htmlContent);
+        return $this->subject('Joni&#39;s Blog: ' . $this->post->title)
+                    ->html($emailHtml);
     }
 }
