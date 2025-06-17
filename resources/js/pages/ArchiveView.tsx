f1e0e62142a6c0c0c147595ab55f731eebe7a8ad
Eyesore123 <joni.putkinen@protonmail.com>
2025-06-17 11:04:54 +0300

    add style fixes

--- resources/js/pages/ArchiveView.tsx
+++ resources/js/pages/ArchiveView.tsx
@@ -85,7 +85,7 @@ export default function ArchiveView() {
       <Header />
       <main className="!p-4 md:!p-8 !gap-1">
           <div className="flex flex-col lg:flex-row gap-4 md:gap-6 xl:gap-10 custom-2xl-gap">
-            <aside className="hidden lg:block w-full sm:!w-1/3 xl:!w-2/3 xl:!ml-20 2xl:!ml-30 xl:max-w-120 xl:!mr-10 !mb-8 lg:!mb-0 mx-auto">
+            <aside className="hidden lg:block w-full md:!w-2/3 xl:!w-2/3 xl:!ml-20 2xl:!ml-30 xl:max-w-120 xl:!mr-10 !mb-8 lg:!mb-0 mx-auto">
               <div className="lg:top-24 !space-y-4 md:!space-y-6 w-full lg:!w-80 xl:!w-120">
                 <div className="rounded-lg bg-[#5800FF]/10 !p-4 2xl:!pl-6">
                 <h3 className="font-semibold !mb-2">About</h3>
