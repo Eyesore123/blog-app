393319e8048ca25b85abaac608628d92bda42e70
Eyesore123 <joni.putkinen@protonmail.com>
2025-09-14 08:46:50 +0300

    add small fix to tags

--- resources/js/components/Tags.tsx
+++ resources/js/components/Tags.tsx
@@ -11,14 +11,14 @@ const TagComponent: React.FC<TagProps> = ({ tags, onTagClick }) => {
       <h3 className="font-semibold !mb-4">Tags</h3>
 
       {/* Grid layout instead of flex */}
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(80px,1fr))] gap-2">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(80px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-2">
         {tags.map((tag) => (
           <button
             key={tag}
             type="button"
             className="bg-[#5800FF] text-white rounded !px-3 !py-1 text-xs text-center 
                       truncate hover:bg-white hover:text-[#5800FF] transition"
-            title={tag} // shows full tag on hover
+            title={tag}
             onClick={() => onTagClick(tag)}
           >
             {tag}
