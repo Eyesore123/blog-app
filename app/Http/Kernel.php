d14dbcd33bde802a8c75a9257f34aef5d47fab00
Eyesore123 <joni.putkinen@protonmail.com>
2025-10-04 18:27:28 +0300

    add fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,8 @@ class Kernel extends HttpKernel
 {
     protected $middlewareGroups = [
     'web' => [
-        EncryptCookies::class,
         RedirectOldDomain::class,
+        EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
